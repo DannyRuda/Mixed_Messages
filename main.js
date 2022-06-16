1c83e8af0a672f52631ea145277e4b75faee5e71
Danny Ruda <danny.ruda1997@gmail.com>
2022-06-16 14:42:09 +0200

    create sets of elements for the sentence components

---
+++ main.js
@@ -0,0 +1,20 @@
+const youtubers = [
+    {
+        name: 'HandofBlood',
+        reactions: ['took his punishment wheel with him', 'lost his mustache out of shock', 'lost his mustache out of enjoyment', 'says "War enttäuschend, bin ich early"', 'is lost in space digga'],
+        pronoun: 'He'
+    },
+    {
+        name: 'Pietsmiet',
+        reactions: ['sticks his tongue out', 'takes a sip from his beer', 'screams agressively'],
+        pronoun: 'He'
+    },
+    {
+        name: 'Mahluna',
+        reactions: ['laughs nervously', 'laughs awkwardly', 'laughs excited', 'smiles desperately'],
+        pronoun: 'She'
+    }
+];
+
+const actions = ['meets an excited Viewer in public','wins the "Deutscher Computerspielpreis"', 'gets a passenger ride in a race car', 'loses the last round in Valorant'];
+
